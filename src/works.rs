36d562cd3dd59e57926d0419177fcbf87b6a873d
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2019-10-06 01:51:04 -0400

    Add other workaround

--- src/works.rs
+++ src/works.rs
@@ -3,3 +3,9 @@ use smallvec::SmallVec; // Don't bring macro into scope
 fn works() {
     let x: SmallVec<[usize; 8]> = SmallVec::new(); // Autocomplete/go to definition works as expected.
 }
+
+fn works2() {
+    use smallvec::smallvec; // Local includes also work for some reason.
+    let mut x: SmallVec<[usize; 3]> = smallvec!(2); // SmallVec stuff still works
+    x.push(3); // Autocomplete still works
+}
